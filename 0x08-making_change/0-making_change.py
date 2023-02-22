99be2b444aa8844719921919fe483a12fa552a03
PROFAY <Oyewaletimmy01@gmail.com>
2023-02-22 12:47:13 +0000

    interview

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
-""" This module determine the fewest number of coin needed
-Return: fewest number of coins needed to meet total
-If total is 0 or less, return 0
-If total cannot be met by any number of coins you have, return -1
+"""
+Given a pile of coins of different values, determine the fewest number of coins
+needed to meet a given amount total
 """
 
 
 def makeChange(coins, total):
-    while total
+    """this functions determines the fewest number of coins needed to meet
+    a given amount total - using coins of different values"""
+    if total <= 0:
+        return 0
+    coins = sorted(coins, reverse=True)
+    curTotal = 0
+    minCoins = 0
+    for coin in coins:
+        bal = (total - curTotal)//coin
+        curTotal += bal*coin
+        minCoins += bal
+        if curTotal == total:
+            return minCoins
+    return -1
