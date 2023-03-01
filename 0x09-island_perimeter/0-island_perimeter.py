37f46a93567b2718815e2a71d1ba4453cb57fa87
PROFAY <Oyewaletimmy01@gmail.com>
2023-03-01 18:04:49 +0000

    perimeter island for only one island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,19 +7,45 @@ calculate the perimeter of the island
 
 
 def island_perimeter(grid):
-    """ Calculate the perimeter of the island
-    """
+    """this function returns the perimeter of the only island described by grid"""
     perimeter = 0
-    grid_length = len(grid)
-    for row in range(grid_length):
+    grid_len = len(grid)
+
+    # Find the location of the only island
+    island_row = None
+    island_col = None
+    for row in range(grid_len):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                if grid[row - 1][col] == 0 or row - 1 < 0:
-                    perimeter += 1
-                if grid[row + 1][col] == 0 or row + 1 >= grid_length:
-                    perimeter += 1
-                if grid[row][col + 1] == 0 or col + 1 >= len(grid[row]):
-                    perimeter += 1
-                if grid[row][col - 1] == 0 or col - 1 < 0:
-                    perimeter += 1
+                island_row = row
+                island_col = col
+                break
+        if island_row is not None:
+            break
+
+    # Traverse the island and count the perimeter
+    stack = [(island_row, island_col)]
+    visited = set()
+    while stack:
+        row, col = stack.pop()
+        if (row, col) in visited:
+            continue
+        visited.add((row, col))
+        if row == 0 or grid[row-1][col] == 0:
+            perimeter += 1
+        if col == 0 or grid[row][col-1] == 0:
+            perimeter += 1
+        if row == grid_len-1 or grid[row+1][col] == 0:
+            perimeter += 1
+        if col == len(grid[row])-1 or grid[row][col+1] == 0:
+            perimeter += 1
+        if row > 0 and grid[row-1][col] == 1:
+            stack.append((row-1, col))
+        if col > 0 and grid[row][col-1] == 1:
+            stack.append((row, col-1))
+        if row < grid_len-1 and grid[row+1][col] == 1:
+            stack.append((row+1, col))
+        if col < len(grid[row])-1 and grid[row][col+1] == 1:
+            stack.append((row, col+1))
+
     return perimeter
